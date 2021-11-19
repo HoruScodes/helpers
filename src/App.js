b78737e71c2eba02b0ff6ba30ad709d20c23e0f8
Rishabhpatel99 <rishabhmpatel99@gmail.com>
2021-11-19 02:28:14 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import "primereact/resources/themes/lara-light-indigo/theme.css"    //theme
-import "primereact/resources/primereact.min.css"                    //core css
-import "primeicons/primeicons.css" 
-
-
-import { Avatar } from 'primereact/avatar';
-import { Badge } from 'primereact/badge';
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import { useState, useEffect } from "react";
+import Profile from "./components/Profile";
+import Socials from "./components/Socials";
+import Footer from "./components/Footer";
 
 function App() {
+  const [profile, setProfile] = useState({
+    name: "404",
+    bio: "-",
+    avatar:
+      "https://avatars.githubusercontent.com/u/83689669?s=400&u=e474f2a5eddc2df7877163dc5fdaef5a4b920edb&v=4",
+    links: [],
+  });
+
+  useEffect(() => {
+    fetch("/data/richgang.json")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log("dsdasdsadasdasdasdas", data);
+        setProfile(data);
+      });
+  }, []);
   return (
-    <div > 
-    <Avatar image="rishabh.JPG" size="xlarge">
-    <Badge value="4" severity="danger" />
-</Avatar>
+    <div className="p-m-4">
+      {/* {console.log("here in componentr", profile)} */}
+      <Profile data={profile} total={profile.links.length} />
+      <Socials links={profile.links} />
+      <Footer />
     </div>
   );
 }
